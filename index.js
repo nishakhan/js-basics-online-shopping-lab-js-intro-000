6ac80abbb51283f04e0ad1413a193047ddd7f054
Nisha Khan <nisha.f.khan@gmail.com>
2020-08-15 23:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,10 +59,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (!cardNumber) {
-    return `Sorry, we don't have a credit card on file for you.`;
+  if (number === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
   } else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+      console.log (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
-  cart.splice(0, cart.length)
+  return cart = []
 }
