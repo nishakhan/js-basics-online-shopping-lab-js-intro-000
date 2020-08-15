e4a36aed9e4ce7ee244ebbb52134b4896a7e4ec8
Nisha Khan <nisha.f.khan@gmail.com>
2020-08-15 23:19:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ function viewCart() {
   // write your code here
   var shoppingItems = []
   for (let i=0; i < cart.length; i++) {
-    var itemName = Object.values(cart[i])[0]
-    var itemPrice = Object.values(cart[i])[1]
-    shoppingItems.push(`${itemName} at $${itemPrice}`)
+    shoppingItems.push(`${cart.itemName} at $${cart.itemPrice}`)
   }
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
